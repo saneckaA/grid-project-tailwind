a3eec90aca6ba97e35b556359d229f0b1ba8fe38
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-10 12:29:41 +0100

    added global container

---
+++ src/GlobalContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GridContainer from '../GridContainer';
+
+const GlobalContainer = () => {
+  return (
+    <div className='container mx-auto max-w-7xl p-2 md:p-10'>
+        <GridContainer />
+    </div>
+  )
+}
+
+export default GlobalContainer;
