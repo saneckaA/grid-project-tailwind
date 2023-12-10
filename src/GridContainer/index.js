d13af1a7906561b081cd0f1b4e0790efadd2e4e2
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-10 12:29:58 +0100

    added grid container

---
+++ src/GridContainer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box1 from '../Box1';
+
+
+const GridContainer = () => {
+    return (
+        <div className='text-white'>
+            <Box1 />
+        </div>
+    )
+}
+
+export default GridContainer;
